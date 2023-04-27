ca18bdd7d9b737b240253af556f14dde9b7f2b71
kailicen <kailicen226@gmail.com>
2023-04-27 12:26:02 +1000

    book

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -14,7 +14,10 @@ function Book({}: Props) {
       className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row
      px-2 md:px-24 justify-evenly mx-auto items-center scroll-smooth"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center">
+      <h3
+        className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center
+      2xl:text-4xl 2xl:top-36"
+      >
         Book
       </h3>
 
@@ -23,18 +26,26 @@ function Book({}: Props) {
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
-        className="flex flex-col space-y-5 md:flex-row md:space-x-5 items-center justify-center z-20"
+        className="flex flex-col space-y-5 md:flex-row md:space-x-5 items-center justify-center z-20
+        max-w-7xl 2xl:flex-row 2xl:space-x-16"
       >
         <Image
           src="/img/book-2.jpg"
           alt="book"
           width={300}
           height={300}
-          className="w-60 h-auto md:w-auto md:h-auto rounded-lg z-20"
+          className="w-60 h-auto md:w-auto md:h-auto rounded-lg z-20
+          2xl:w-auto 2xl:h-[300px]"
         />
-        <div className="flex flex-col space-y-3 md:space-y-5 items-start md:text-lg z-20">
-          <h2 className="text-lg md:text-xl">
-            <span className="decoration-green-400 underline">
+        <div
+          className="flex flex-col space-y-3 md:space-y-5 items-start md:text-lg z-20
+        2xl:text-2xl"
+        >
+          <h2
+            className="text-lg md:text-xl
+          2xl:text-3xl"
+          >
+            <span className="decoration-emerald-600 underline">
               A Heartfelt Undertaking
             </span>{" "}
           </h2>
@@ -47,7 +58,11 @@ function Book({}: Props) {
           <p className="hidden md:block">
             Click the learn more button to know more about Bec&apos;s book.
           </p>
-          <button onClick={() => router.push("/book")} className="button">
+          <button
+            onClick={() => router.push("/book")}
+            className="button
+          text-lg"
+          >
             Learn More
           </button>
         </div>
